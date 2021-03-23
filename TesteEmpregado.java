9806e2697c435bb0554d402e8a645c0dc020690d
‘Caio <caiofmsampaio@gmail.com>
2021-03-23 16:49:10 -0300

    commit das classes

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -3,4 +3,12 @@ public class TesteEmpregado{
         Empregado e1 = new Empregado ();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado ();
+
+        e1.setTipo(1);
+        e2.setTipo(2);
+        e3.setTipo(3);
+
+        e1.setSalario(2000);
+        e2.setSalario(1700);
+        e2.setComissao(0.2);
     }
